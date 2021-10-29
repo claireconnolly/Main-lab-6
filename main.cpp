7278f040e32febcee66a314ba134cce592b205f4
claireconnolly <93167283+claireconnolly@users.noreply.github.com>
2021-10-28 18:57:55 -0600

    Added a bunch of stuff

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <fstream>
+
 using namespace std;
 
 const int HOTPLATE_SIZE = 10;
 
 void HeatRow(int row, double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE]);
-bool UpdateElements(double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE], int iteration = 0);
+bool UpdateElements(double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE]);
 void CopyHotPlate(double (&newHotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE], double (&oldHotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE]);
 void PrintHotPlate(double hotPlate[HOTPLATE_SIZE][HOTPLATE_SIZE]);
 bool CheckSteadyState(double oldHotPlate[HOTPLATE_SIZE][HOTPLATE_SIZE], double newHotPlate[HOTPLATE_SIZE][HOTPLATE_SIZE]);
@@ -30,11 +32,76 @@ int main() {
     int iteration = 0;
 
     do {
-        steadyStateAchieved = UpdateElements(hotPlate, iteration);
+        steadyStateAchieved = UpdateElements(hotPlate);
+
+        // Print hot plate when required
+        if(!steadyStateAchieved) {
+            if(iteration == 0) {
+                // first iteration
+                cout << "Printing plate after one iteration..." << endl;
+                // Print out hotplate
+                PrintHotPlate(hotPlate);
+            }
+        } else {
+            // Final hot plate
+            cout << "Printing final plate..." << endl;
+            // Print out hotplate
+            PrintHotPlate(hotPlate);
+        }
+
         iteration++;
     } while (steadyStateAchieved == false);
 
+
+    // output into file
+
+    cout << "Writing final plate to \"Hotplate.csv.\"..." << endl << endl;
+    ofstream out;
+    out.open("Hotplate.csv");
+
+    if (out.is_open()){
+        for(int i = 0; i < HOTPLATE_SIZE; ++i) {
+            for(int j = 0; j < HOTPLATE_SIZE; ++j) {
+                out << fixed << setprecision(3) << setw(9) << hotPlate[i][j];
+
+                if (j < HOTPLATE_SIZE - 1){
+                    out << ",";
+                }
+            }
+            out << endl;
+        }
+        out << endl;
+
+    }
+
+    out.close();
+
+    //input test from file
+
+    ifstream in;
+    in.open("Inputplate.txt");
+
+    // Initialize matrix with 0
+    double inputPlate[HOTPLATE_SIZE][HOTPLATE_SIZE] = {0};
+
+    // Load input plate
+    for (int i = 0; i < HOTPLATE_SIZE; ++i){
+        for (int j = 0; j < HOTPLATE_SIZE; ++j){
+            in >> inputPlate[i][j];
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        UpdateElements(inputPlate);
+    }
+
+    cout << "Printing input plate after 3 updates..." << endl;
+    PrintHotPlate(inputPlate);
+
+    in.close();
+
     return 0;
+
 }
 
 // Heat row, with corners = 0 and insides = 100
@@ -44,7 +111,7 @@ void HeatRow(int row, double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE]) {
     }
 }
 
-bool UpdateElements(double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE], int iteration) {
+bool UpdateElements(double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE]) {
     double newHotPlate[HOTPLATE_SIZE][HOTPLATE_SIZE] = {0};
 
     CopyHotPlate(hotPlate, newHotPlate);
@@ -57,27 +124,10 @@ bool UpdateElements(double (&hotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE], int iterat
 
     bool steadyStateAchieved = CheckSteadyState(hotPlate, newHotPlate);
 
-    if(!steadyStateAchieved) {
-        if(iteration == 0) {
-            // first iteration
-            cout << "Printing plate after one iteration..." << endl;
-            // Print out hotplate
-            PrintHotPlate(newHotPlate);
-        }
+    // Copy new hotplate into old hotplate
+    CopyHotPlate(newHotPlate, hotPlate);
 
-        // Copy new hotplate into old hotplate
-        CopyHotPlate(newHotPlate, hotPlate);
-
-        return false;
-    } else {
-        // Final hot plate
-        cout << "Printing final plate..." << endl;
-
-        // Print out hotplate
-        PrintHotPlate(newHotPlate);
-
-        return true;
-    }
+    return steadyStateAchieved;
 }
 
 void CopyHotPlate(double (&newHotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE], double (&oldHotPlate)[HOTPLATE_SIZE][HOTPLATE_SIZE]) {
